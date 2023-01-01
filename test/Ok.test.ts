b82422d524132a109823f2b320020fd31941571c
adil <68734155+adevinwild@users.noreply.github.com>
2023-01-01 18:46:49 +0100

    feat: :sparkles: Replacement of dedicated arguments for an options object
    All responses classes can have an options objet as constructor that use a tweaked version of IHttpResponse (Without statusCode and defaultMessage)
    
    - BREAKING-CHANGE: All classes

--- test/Ok.test.ts
+++ test/Ok.test.ts
@@ -2,14 +2,12 @@ import { Ok } from '../src';
 
 describe('Ok', () => {
   it('should correctly serialize to a JSON object', () => {
-    const ok = new Ok(
-      'Everything is fine.',
-      {
-        foo: 'bar',
-      },
-      'https://example.com/docs/ok',
-      '#ref-123'
-    );
+    const ok = new Ok({
+      cause: 'Everything is fine.',
+      metadata: { foo: 'bar' },
+      reference: 'https://example.com/docs/ok',
+      requestId: '#ref-123',
+    });
     const expectedJSON = {
       statusCode: 200,
       message: 'OK',
@@ -25,50 +23,35 @@ describe('Ok', () => {
 
   it('should return a 200 response', () => {
     const response = new Ok();
-
     expect(response.statusCode).toBe(200);
     expect(response.message).toBe('OK');
   });
 
   it('should return a 200 response with a cause if it is provided', () => {
-    const response = new Ok('Everything is fine.');
+    const response = new Ok({ cause: 'Everything is fine.' });
     expect(response.statusCode).toBe(200);
     expect(response.message).toBe('OK');
     expect(response.cause).toBe('Everything is fine.');
   });
 
   it("should return a 200 response with metadata if it's provided", () => {
-    const response = new Ok('Everything is fine.', {
-      foo: 'bar',
-    });
+    const response = new Ok({ metadata: { foo: 'bar' } });
     expect(response.statusCode).toBe(200);
     expect(response.message).toBe('OK');
-    expect(response.cause).toBe('Everything is fine.');
     expect(response.metadata).toEqual({ foo: 'bar' });
   });
 
   it('should return a 200 response with a reference if it is provided', () => {
-    const response = new Ok(
-      'Everything is fine.',
-      undefined,
-      'https://example.com/docs/ok'
-    );
+    const response = new Ok({ reference: 'https://example.com/docs/ok' });
     expect(response.statusCode).toBe(200);
     expect(response.message).toBe('OK');
-    expect(response.cause).toBe('Everything is fine.');
     expect(response.reference).toBe('https://example.com/docs/ok');
   });
 
   it('should return a 200 response with a requestId if it is provided', () => {
-    const response = new Ok(
-      'Everything is fine.',
-      undefined,
-      undefined,
-      '#ref-123'
-    );
+    const response = new Ok({ requestId: '#ref-123' });
     expect(response.statusCode).toBe(200);
     expect(response.message).toBe('OK');
-    expect(response.cause).toBe('Everything is fine.');
     expect(response.requestId).toBe('#ref-123');
   });
 });

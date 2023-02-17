905a31df052a8b0f077941b0f80c83a1997c7c31
adil <68734155+adevinwild@users.noreply.github.com>
2023-02-17 08:44:14 +0100

    refactor: :recycle: Refactored HttpResponse with the goal to remove useless abstractions

--- src/core/HttpResponse.ts
+++ src/core/HttpResponse.ts
@@ -1,20 +1,43 @@
 import ConfigFileFetcher from './ConfigFileFetcher';
-import {
-  ExpressRes,
-  FastifyRes,
-  HapiRes,
-  HttpRes,
-  IHttpResponse,
-  KoaRes,
-} from './interface';
 
-/**
- * Base class for a premade HTTP response.
- * @abstract
- * @class HttpResponseBase
- * @implements {IHttpResponse<T>}
- */
-export default abstract class HttpResponseBase<T> {
+export type HttpResponseOptions<T> = {
+  /**
+   * The HTTP status code.
+   * @type { number }
+   */
+  statusCode: number;
+
+  /**
+   * The response message.
+   * @type { string }
+   */
+  message: string;
+
+  /**
+   * **The optional cause message.**
+   * @type { string }
+   */
+  cause?: string;
+
+  /**
+   * **Custom metadata to be included in the response.**
+   */
+  metadata?: T;
+
+  /**
+   * **The optional reference to more detailed documentation or information about the response.**
+   * @type {string}
+   */
+  reference?: string;
+
+  /**
+   * The optional request ID for tracking and debugging purposes.
+   * @type {string}
+   */
+  requestId?: string;
+};
+
+export class HttpResponse<T> {
   readonly statusCode: number;
   readonly message: string;
   readonly cause?: string;
@@ -22,7 +45,7 @@ export default abstract class HttpResponseBase<T> {
   readonly reference?: string;
   readonly requestId?: string;
 
-  constructor(response: IHttpResponse<T>) {
+  constructor(response: HttpResponseOptions<T>) {
     this.statusCode = response.statusCode;
     this.message = response.message;
     this.metadata = response.metadata;
@@ -31,7 +54,7 @@ export default abstract class HttpResponseBase<T> {
     this.reference = response.reference;
   }
 
-  public async send(serverResponse: unknown): Promise<unknown> {
+  public async send(serverResponse: unknown) {
     const config = await new ConfigFileFetcher().fetchConfig();
 
     switch (config.serverType) {
@@ -69,3 +92,32 @@ export default abstract class HttpResponseBase<T> {
     return JSON.stringify(this);
   }
 }
+
+export function createHttpResponse<T>(response: HttpResponseOptions<T>) {
+  return new HttpResponse(response);
+}
+
+export type ExpressRes = {
+  status: (code: number) => ExpressRes;
+  json: <T>(data: T) => any;
+};
+
+export type HttpRes = {
+  writeHead: (code: number, headers: Record<string, string>) => HttpRes;
+  end: (data: any) => any;
+};
+
+export type FastifyRes = {
+  code: (code: number) => FastifyRes;
+  send: <T>(data: T) => any;
+};
+
+export type KoaRes = {
+  status: number;
+  body: unknown | HttpResponse<unknown>;
+};
+
+export type HapiRes = {
+  response: <T>(data: T) => HapiRes;
+  code: (code: number) => HapiRes;
+};
